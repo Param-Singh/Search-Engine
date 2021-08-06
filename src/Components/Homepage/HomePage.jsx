f5f14b31ace34bdb2e4a553e91df7cf2a2f96189
Param-Singh <isinghparamjeet@gmail.com>
2021-08-06 21:12:23 +0530

    added speech to text search feature

--- src/Components/Homepage/HomePage.jsx
+++ src/Components/Homepage/HomePage.jsx
@@ -4,9 +4,11 @@ import  '../../Css/Homepage.css';
 import React from 'react'
 import { useHistory } from "react-router-dom";
 import VoiceRecognisition from './VoiceRecognisition';
+import { useState } from 'react';
 export default function HomePage({queryFetcher}) {
     var query=''
     let history = useHistory();
+    const [VoiceComponentVisibility,setVoiceComponentVisibility]=useState(false);
     function callbackQuery (Receivedquery) {
         query=Receivedquery;
         console.log(query)
@@ -21,13 +23,14 @@ export default function HomePage({queryFetcher}) {
             <div id='stars'></div>
             <div id='stars2'></div>
             <div id='stars3'></div>
-        <div className='whole'>
-            <div className="horizontal">
-            <Logo />
-            <SearchBar q={query} callbackQuery={callbackQuery}/>
-            <VoiceRecognisition/>
+            <div className='whole'>
+                <div className="horizontal">
+                <Logo />
+                <SearchBar q={query} callbackQuery={callbackQuery}/>
+                <button className="searchButton" onClick={()=>setVoiceComponentVisibility(!VoiceComponentVisibility)}>Voice Search</button>
+                {VoiceComponentVisibility ? <VoiceRecognisition q={query} callbackQuery={callbackQuery}/> : <span></span>}
+                </div>
             </div>
-        </div>
         </div>
     )
 }
--- src/Components/Homepage/VoiceRecognisition.jsx
+++ src/Components/Homepage/VoiceRecognisition.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
-
-export default function VoiceRecognisition() {
+import '../../Css/Homepage.css';
+export default function VoiceRecognisition({q,callbackQuery}) {
     navigator.mediaDevices.getUserMedia({ audio: true })
       .then(function(stream) {
         console.log('You let me use your mic!')
@@ -17,15 +17,21 @@ export default function VoiceRecognisition() {
       } = useSpeechRecognition();
     const listenContinously = () => SpeechRecognition.startListening({ continuous: true,language: 'en-GB' });
       if (!browserSupportsSpeechRecognition) {
-        return <span>Browser doesn't support speech recognition.</span>;
+        return <span>Browser doesn't support speech recognition. Please use Chrome</span>;
+      }
+      function handleSearch() {
+        callbackQuery(transcript)
+        console.log(transcript)
       }
     return (
-        <div>
-            <p>Microphone: {listening ? 'on' : 'off'}</p>
-            <button
+        <div className='voice'>
+            <p className='mic'>Microphone: {listening ? 'on' : 'off'}</p>
+            {listening ? <button className="searchButton2"
+            onClick={handleSearch}
+            >Search</button> : <button className="searchButton2"
             onClick={listenContinously}
-            >{listening ? 'Click On Search': 'Hold To Talk'}</button>
-            <p>{transcript}</p>
+            >'Click To Talk'</button>}
+            <p className='transcript'>{transcript}</p>
         </div>
     )
 }
